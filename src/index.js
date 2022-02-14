439d97b60c9fa94fb07ba5d1398c269b744c736a
Valentyn Kovalchuk <kovalhood@gmail.com>
2022-02-14 17:35:55 +0200

    Layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-import './sass/main.scss';
+import './css/styles.css';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+const refs = {
+    form: document.querySelector('form#search-form'),
+    inputField: document.querySelector('input.search-form__input'),
+    searchButton: document.querySelector('button.search-form__button'),
+    gallery: document.querySelector('div.gallery'),
+    loadMoreButton: document.querySelector('button.load-more'),
+};
+
+console.log(refs.form, refs.inputField, refs.searchButton, refs.gallery, refs.loadMoreButton);
+
+const DEBOUNCE_DELAY = 300;
